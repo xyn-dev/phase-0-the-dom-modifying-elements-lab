bcb002b095ede4b1770bb4160c5d040a0a648707
Hunter Wade <calebh.wade@gmail.com>
2023-05-05 18:30:41 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory");
+
